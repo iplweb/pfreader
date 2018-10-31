9e4d8995a62ba154f182f4eb05a0655ff8c6abca
Michał Pasternak <michal.dtz@gmail.com>
2018-10-31 19:55:50 +0100

    0.0.3-dev

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = reqs("requirements_dev.txt")
 
 setup(
     name='pfreader',
-    version='0.0.2',
+    version='0.0.3-dev',
     description="Reader for Baxter® PrismaFlex® LOX files",
     long_description=readme + '\n\n' + history,
     author="IPLWeb",
